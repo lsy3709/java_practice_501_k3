149b801e0b7df53e8887a44e974cf60611b9b7d0
leesanghwa1997 <zlqlcm1997@gmail.com>
2024-10-21 16:58:51 +0900

    이상화 7교시 숙제(음식 클래스정의 및 함수이용)

---
+++ Java_Practice2/src/ex_241021_ch4/homework/Lsh1208_homework2_Food.java
@@ -0,0 +1,43 @@
+package ex_241021_ch4.homework;
+
+import java.util.Random;
+
+class Food {
+	String name;
+	int price;
+	String material[];
+
+	Food(String name, int price, String[] material) {
+		this.name = name;
+		this.price = price;
+		this.material = material;
+	}
+
+	void introduceFoods() {
+		System.out.println("음식이름: " + name + "\n가격: " + price);
+		System.out.print("재료:");
+		for (int i = 0; i < material.length; i++) {
+			System.out.print(material[i] + " ");
+		}
+		System.out.println();
+	}
+
+	void randomFoods(Food[] foods) {
+		Random random = new Random();
+		int randomIndex = random.nextInt(foods.length);
+		System.out.println("랜덤하게 선택된 음식:");
+		foods[randomIndex].introduceFoods();
+	}
+}
+
+public class Lsh1208_homework2_Food {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Food lshFood1 = new Food("햄버거", 5900, new String[] { "치즈", "빵" });
+		Food lshFood2 = new Food("피자", 12000, new String[] { "치즈", "도우", "토마토 소스" });
+		Food lshFood3 = new Food("스파게티", 8500, new String[] { "면", "소스", "고기" });
+		lshFood1.introduceFoods();
+
+		lshFood1.randomFoods(new Food[] { lshFood1, lshFood2, lshFood3 });
+	}
+}
